b0823167a6177f11d56417dadfc064f965a0ddda
Facuscar <facuscardillo@gmail.com>
2022-12-01 22:10:56 -0300

    Retrieve news from the API

--- context/NewsProvider.jsx
+++ context/NewsProvider.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { useState, useEffect, createContext } from 'react';
 
 export const NewsContext = createContext();
@@ -5,6 +6,18 @@ export const NewsContext = createContext();
 const NewsProvider = ({children}) => {
 
     const [category, setCategory] = useState('general');
+    const [news, setNews] = useState([]);
+
+    useEffect(() => {
+        const getNews = async () => {
+            const url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`
+
+            const { data } = await axios(url);
+            
+            console.log(data);
+        }
+        getNews();
+    }, [category]);
 
     const handleCategoryChange = (e) => {
         setCategory(e.target.value);
